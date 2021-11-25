f78be42c8a5e32adb6bf66f81b73f50fda035166
Tutorials-Git <ankireddy905292@gmail.com>
2021-11-25 14:37:36 +0530

    Generics

--- src/com/generics/FindMax.java
+++ src/com/generics/FindMax.java
@@ -4,4 +4,11 @@ public class FindMax {
     public static void main(String[] args) {
         System.out.println("Generics");
     }
+
+    public Integer findMaxValue(int i, int i1, int i2) {
+        return 215;
+    }
 }
+
+
+
